f8ccac59076d3e9eee8b984030c6f1e10d3d189a
海投_郑科 <2942028059@qq.com>
2019-07-15 10:15:57 +0800

    广告页

---
+++ lib/ui/pages/splash_page.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+
+
+class SplashPage extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return new _SplashPageState();
+  }
+}
+
+class _SplashPageState extends State<SplashPage>{
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Text("ggggg");
+  }
+
+}
